ff336dfeaba689df4488e1dd8d18dd3c99f091cd
labouz <lb3338@drexel.edu>
2021-12-01 16:19:28 -0500

    TRAN-46 ; modify description, document

--- R/get_pmcid.R
+++ R/get_pmcid.R
@@ -1,3 +1,13 @@
+#' Convert PMIDs to PMCIDs
+#'
+#' @param ... multiple strings holding possible drug names
+#'
+#' @return A named list of PMCIDs with the input PMIDs
+#' @importFrom jsonlite fromJSON
+#' @export
+#'
+#' @examples
+#' get_pmcid(19023454, "7016449")
 get_pmcid <- function(...){
   # ... : vector of integers, chars, if more than one pmid supplied not as a vector
   id_ls = list(...)
@@ -10,23 +20,24 @@ get_pmcid <- function(...){
   base_url <- "https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?versions=no&format=json&idtype=pmid&"
 
   id_to_pmc <- lapply(id_to_char, function(x){
-
     # paste pmid to url
     pmid_vec <- paste0("ids=", x)
+
     url <- paste0(base_url, pmid_vec)
-    # call to API that returns JSON
+
     convert <- fromJSON(url)
     # extract PMCID
     PMCID <- convert$records$pmcid
 
     if(is.null(PMCID)){
-      c(PMID = x, PMCID = "invalid")
+      "invalid"
     }else{
-      c(PMID = x, PMCID = PMCID)
+      PMCID
     }
   }
   )
 
+  names(id_to_pmc) <- id_to_char
   id_to_pmc
 
 }
